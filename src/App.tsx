663e837dae7643178607b526acb6f25f2b22ba37
Pawan Paudel <pawanpaudel93@gmail.com>
2022-09-14 16:37:28 +0545

    updated homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import MdEditor from "md-editor-rt";
+import "md-editor-rt/lib/style.css";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState(`## web3-action-deploy
+
+  An example of react app deployment using github action.`);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <MdEditor
+      modelValue={text}
+      onChange={setText}
+      language="en-US"
+      pageFullScreen={true}
+    />
+  );
 }
 
-export default App
+export default App;
