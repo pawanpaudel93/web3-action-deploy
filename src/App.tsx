2c59ae235cd323d52334c8cc1c8e2917bb62ec51
Pawan Paudel <pawanpaudel93@gmail.com>
2022-09-16 10:18:55 +0545

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { useEffect, useState } from "react";
 import MdEditor from "md-editor-rt";
 import "md-editor-rt/lib/style.css";
-import "./App.css";
 
 function App() {
   const [text, setText] = useState(``);
 
   const loadText = async () => {
-    const _text = await (await fetch("/content.md")).text();
-    setText(_text);
+    setText(await (await fetch("/content.md")).text());
   };
 
   useEffect(() => {
